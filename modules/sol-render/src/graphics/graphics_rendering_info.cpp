cd2fec0430573a3ebfb151f833e8ddec930765cb
TimZoet <ts.zoet@gmail.com>
2023-07-16 20:52:47 +0200

    Added begin/end rendering and image transitions to the GraphicsRenderingInfo class.

--- modules/sol-render/src/graphics/graphics_rendering_info.cpp
+++ modules/sol-render/src/graphics/graphics_rendering_info.cpp
@@ -5,12 +5,16 @@
 ////////////////////////////////////////////////////////////////
 
 #include <algorithm>
+#include <ranges>
 
 ////////////////////////////////////////////////////////////////
 // Module includes.
 ////////////////////////////////////////////////////////////////
 
+#include "sol-core/vulkan_command_buffer.h"
+#include "sol-core/vulkan_image.h"
 #include "sol-core/vulkan_image_view.h"
+#include "sol-core/vulkan_queue_family.h"
 #include "sol-error/sol_error.h"
 #include "sol-material/graphics/graphics_material.h"
 
@@ -125,6 +129,37 @@ namespace sol
         addColorAttachment(imageView, imageLayout, loadOp, storeOp, clear);
     }
 
+    void GraphicsRenderingInfo::setColorAttachmentPreTransition(const size_t                index,
+                                                                const VulkanQueueFamily*    srcQueue,
+                                                                const VulkanQueueFamily*    dstQueue,
+                                                                const VkImageLayout         oldLayout,
+                                                                const VkImageLayout         newLayout,
+                                                                const VkPipelineStageFlags2 srcStage,
+                                                                const VkPipelineStageFlags2 dstStage,
+                                                                const VkAccessFlags2        srcAccess,
+                                                                const VkAccessFlags2        dstAccess)
+    {
+        requireNonFinalized();
+
+        preColorTransitions.at(index) =
+          ColorAttachmentTransition{srcQueue, dstQueue, oldLayout, newLayout, srcStage, dstStage, srcAccess, dstAccess};
+    }
+
+    void GraphicsRenderingInfo::setColorAttachmentPostTransition(const size_t                index,
+                                                                 const VulkanQueueFamily*    srcQueue,
+                                                                 const VulkanQueueFamily*    dstQueue,
+                                                                 const VkImageLayout         oldLayout,
+                                                                 const VkImageLayout         newLayout,
+                                                                 const VkPipelineStageFlags2 srcStage,
+                                                                 const VkPipelineStageFlags2 dstStage,
+                                                                 const VkAccessFlags2        srcAccess,
+                                                                 const VkAccessFlags2        dstAccess)
+    {
+        requireNonFinalized();
+        postColorTransitions.at(index) =
+          ColorAttachmentTransition{srcQueue, dstQueue, oldLayout, newLayout, srcStage, dstStage, srcAccess, dstAccess};
+    }
+
     void GraphicsRenderingInfo::addDepthAttachment(const VulkanImageView&    imageView,
                                                    const VkImageLayout       imageLayout,
                                                    const VkAttachmentLoadOp  loadOp,
@@ -176,6 +211,8 @@ namespace sol
         requireNonFinalized();
 
         colorImageViews.emplace_back(&imageView);
+        preColorTransitions.emplace_back();
+        postColorTransitions.emplace_back();
 
         VkClearValue clear;
         clear.color = clearColor;
@@ -204,5 +241,74 @@ namespace sol
         renderingInfo.pColorAttachments    = colorAttachmentInfo.data();
         renderingInfo.pDepthAttachment     = depthAttachmentInfo ? &depthAttachmentInfo.value() : VK_NULL_HANDLE;
         renderingInfo.pStencilAttachment   = stencilAttachmentInfo ? &stencilAttachmentInfo.value() : VK_NULL_HANDLE;
+
+        // Setup barriers.
+        for (const auto [image, transition] :
+             std::views::zip(colorImageViews, preColorTransitions) |
+               std::views::filter([](const auto& pair) -> bool { return std::get<1>(pair).has_value(); }))
+        {
+            preBarriers.emplace_back(VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER_2,
+                                     nullptr,
+                                     transition->srcStage,
+                                     transition->srcAccess,
+                                     transition->dstStage,
+                                     transition->dstAccess,
+                                     transition->oldLayout,
+                                     transition->newLayout,
+                                     transition->srcQueue ? transition->srcQueue->getIndex() : VK_QUEUE_FAMILY_IGNORED,
+                                     transition->dstQueue ? transition->dstQueue->getIndex() : VK_QUEUE_FAMILY_IGNORED,
+                                     image->getImage().get(),
+                                     VkImageSubresourceRange{VK_IMAGE_ASPECT_COLOR_BIT, 0, 1, 0, 1});
+        }
+        for (const auto [image, transition] :
+             std::views::zip(colorImageViews, postColorTransitions) |
+               std::views::filter([](const auto& pair) -> bool { return std::get<1>(pair).has_value(); }))
+        {
+            postBarriers.emplace_back(VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER_2,
+                                      nullptr,
+                                      transition->srcStage,
+                                      transition->srcAccess,
+                                      transition->dstStage,
+                                      transition->dstAccess,
+                                      transition->oldLayout,
+                                      transition->newLayout,
+                                      transition->srcQueue ? transition->srcQueue->getIndex() : VK_QUEUE_FAMILY_IGNORED,
+                                      transition->dstQueue ? transition->dstQueue->getIndex() : VK_QUEUE_FAMILY_IGNORED,
+                                      image->getImage().get(),
+                                      VkImageSubresourceRange{VK_IMAGE_ASPECT_COLOR_BIT, 0, 1, 0, 1});
+        }
+
+        // Setup dependency infos.
+        if (!preBarriers.empty())
+        {
+            preDependencyInfo.imageMemoryBarrierCount = static_cast<uint32_t>(preBarriers.size());
+            preDependencyInfo.pImageMemoryBarriers    = preBarriers.data();
+        }
+        if (!postBarriers.empty())
+        {
+            postDependencyInfo.imageMemoryBarrierCount = static_cast<uint32_t>(postBarriers.size());
+            postDependencyInfo.pImageMemoryBarriers    = postBarriers.data();
+        }
     }
+
+    void GraphicsRenderingInfo::beginRendering(const VulkanCommandBuffer& buffer) const
+    {
+        vkCmdBeginRendering(buffer.get(), &renderingInfo);
+    }
+
+    void GraphicsRenderingInfo::endRendering(const VulkanCommandBuffer& buffer) const
+    {
+        vkCmdEndRendering(buffer.get());
+    }
+
+    void GraphicsRenderingInfo::preTransition(const VulkanCommandBuffer& buffer) const
+    {
+        if (!preBarriers.empty()) vkCmdPipelineBarrier2(buffer.get(), &preDependencyInfo);
+    }
+
+    void GraphicsRenderingInfo::postTransition(const VulkanCommandBuffer& buffer) const
+    {
+        if (!postBarriers.empty()) vkCmdPipelineBarrier2(buffer.get(), &postDependencyInfo);
+    }
+
 }  // namespace sol
